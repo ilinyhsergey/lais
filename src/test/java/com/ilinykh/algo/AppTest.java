b80b587c3eb3b3729003bf3a74debfaff45e91ec
Valery Ilinykh <valery0056@gmail.com>
2016-04-17 19:56:33 +0600

    test

--- src/test/java/com/ilinykh/algo/AppTest.java
+++ src/test/java/com/ilinykh/algo/AppTest.java
@@ -1,38 +1,15 @@
 package com.ilinykh.algo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void validateLais(){
+        assertEquals(0,0);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
